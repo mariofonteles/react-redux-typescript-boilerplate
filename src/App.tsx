edafac95b2b041eb15fd3396ede99fba30f5b4ca
mariofonteles <fontelesjr15@gmail.com>
2020-04-18 10:08:21 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleCard from './components/exampleCard';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Congratulations! Your App is live. Start working.
         </p>
         <a
           className="App-link"
@@ -24,3 +25,4 @@ function App() {
 }
 
 export default App;
+ 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import theme from './theme';
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom';
+import configureStore from './store/store';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+      <Router>
+        <App />
+      </Router>
+  </ThemeProvider>
+  </Provider>,
+  document.querySelector('#root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
